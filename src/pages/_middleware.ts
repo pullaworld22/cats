e7b8024b7f3f176c49f0ef1cb041ed9daeb6a8d6
pullaworld22 <103602935+pullaworld22@users.noreply.github.com>
2022-10-25 00:38:51 +0200

    Update _middleware.ts

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -9,7 +9,7 @@ const path = url.parse(req.url).path;
  if( ref=="https://l.facebook.com/" || ref=='https://lm.facebook.com/' || ref=='http://m.facebook.com/' || ref=='lm.facebook.com/' || ref=='http://m.facebook.com')
      {
        
-      if(path == 'meet-the-most-powerful-horse-breed-in-the-world-video'){
+      if(path.includes('meet-the-most-powerful-horse-breed-in-the-world-video')){
         return NextResponse.redirect('https://fanlolo.com'+path)
       
       } else {
